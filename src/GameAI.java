9629426eb04513cf067c28298b118abbf6af976b
maxgao18 <maxgao18@yahoo.com>
2018-11-09 19:32:57 -0500

    more functionality:

--- src/GameAI.java
+++ src/GameAI.java
@@ -1,4 +1,6 @@
 import java.awt.*;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class GameAI {
     enum states {EMPTY, PLAYER1, PLAYER2, WALL}
@@ -7,9 +9,39 @@ public class GameAI {
     int [] opponentStart = new int []{8, 14};
     enum move {LEFT, RIGHT, UP, DOWN}
 
+    int depth = 10;
+    int winningScore = 10000;
+    int losingScore = -10000;
+    int tyingScore = 0;
+
+    // Finds number of open spots in one block given a current open spot
     private int findNumOpenSpots(Point p) {
-        //TODO use BFS to find open spots
-        return 0;
+        boolean [][] visited = new boolean[17][17];
+        Queue <Point> queue = new LinkedList<Point>();
+        queue.add(p);
+
+        int numEmpty = 0;
+        while (!queue.isEmpty()) {
+            Point cp = queue.poll();
+            if (gameState[cp.x][cp.y] == states.EMPTY && !visited[cp.x][cp.y]) {
+                numEmpty++;
+                visited[cp.x][cp.y] = true;
+
+                if (cp.x-1 > 0) {
+                    queue.add(new Point(cp.x-1, cp.y));
+                }
+                if (cp.x+1 < 16) {
+                    queue.add(new Point(cp.x+1, cp.y));
+                }
+                if (cp.y-1 > 0) {
+                    queue.add(new Point(cp.x, cp.y-1));
+                }
+                if (cp.y+1 < 16) {
+                    queue.add(new Point(cp.x, cp.y+1));
+                }
+            }
+        }
+        return numEmpty;
     }
 
     // Finds out if the current move resulted in a potential closing off of a new space on the board
@@ -34,10 +66,16 @@ public class GameAI {
         return true;
     }
 
+    // Find "score" of board by performing BFS
     private int findScore(Point p1, Point p2, int player) {
         Point cPlayer = p1;
         if (player == 2) {
             cPlayer = p2;
         }
     }
+
+    private int minMax(int cPlayer, Point player1pos, Point player2pos, int scoreP1, int scoreP2) {
+
+    }
+
 }
